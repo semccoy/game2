032a192857f230f2134c8776b0897c1a82a841af
semccoy <semccoy@vassar.edu>
2014-11-12 10:43:42 -0500

    init

---
+++ src/game2/Game2.java
@@ -0,0 +1,16 @@
+package game2;
+
+// move screen with player? (return screen +/- char location on a bigger world screen) 
+// ssbb lvl?
+// random slidey lvl?
+// shooting lvl?
+// triggerable powerup things
+// doge bullet bill-esque lvls where projectiles home towards you
+
+public class Game2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
